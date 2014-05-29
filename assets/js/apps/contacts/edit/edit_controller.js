104e2cad1c93808192a59f7037456069cfc6c6f8
Felicia Larion <felicialarion@reea.net>
2014-05-29 17:39:59 +0300

    Saving and validating

--- assets/js/apps/contacts/edit/edit_controller.js
+++ assets/js/apps/contacts/edit/edit_controller.js
@@ -6,12 +6,22 @@ ContactManager.module("ContactsApp.Edit", function(Edit, ContactManager, Backbon
 
 			var fetchingContact = ContactManager.request("contact:entity",id);
 		    $.when(fetchingContact).done(function(contact){
+		    	var contactEdit;
 		        if(contact === undefined){
-		          var contactEdit = new ContactManager.ContactsApp.List.NoContact();
+		          contactEdit = new ContactManager.ContactsApp.List.NoContact();
 		        } 
 		        else{
-		          var contactEdit = new Edit.Contact({model: contact});
+		          contactEdit = new Edit.Contact({model: contact});
 		        }
+
+		        contactEdit.on("form:submit",function(data){
+		        	if(contact.save(data)){
+		        		ContactManager.trigger("contact:show", contact.get("id"));
+		        	}
+		        	else{
+		        		contactEdit.triggerMethod("form:data:invalid", contact.validationError);
+		        	}
+		        });
 
 		        ContactManager.mainRegion.show(contactEdit);
 		      })
